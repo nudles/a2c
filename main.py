8304696fc154b985450e402f6859781f0e99614c
Wang Wei <dcswaw@nus.edu.sg>
2018-03-27 00:18:01 +0800

    no grammar errors; nan loss

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
@@ -20,6 +19,7 @@ from kfac import KFACOptimizer
 from model import CNNPolicy, MLPPolicy
 from storage import RolloutStorage
 from visualize import visdom_plot
+import rafiki
 
 args = get_args()
 
@@ -59,7 +59,8 @@ def main():
     obs_shape = (obs_shape[0] * args.num_stack, *obs_shape[1:])
 
     if len(envs.observation_space.shape) == 3:
-        actor_critic = CNNPolicy(obs_shape[0], envs.action_space, args.recurrent_policy)
+        actor_critic = CNNPolicy(
+            obs_shape[0], envs.action_space, args.recurrent_policy)
     else:
         assert not args.recurrent_policy, \
             "Recurrent policy is not implemented for the MLP controller"
@@ -74,13 +75,16 @@ def main():
         actor_critic.cuda()
 
     if args.algo == 'a2c':
-        optimizer = optim.RMSprop(actor_critic.parameters(), args.lr, eps=args.eps, alpha=args.alpha)
+        optimizer = optim.RMSprop(actor_critic.parameters(
+        ), args.lr, eps=args.eps, alpha=args.alpha)
     elif args.algo == 'ppo':
-        optimizer = optim.Adam(actor_critic.parameters(), args.lr, eps=args.eps)
+        optimizer = optim.Adam(actor_critic.parameters(),
+                               args.lr, eps=args.eps)
     elif args.algo == 'acktr':
         optimizer = KFACOptimizer(actor_critic)
 
-    rollouts = RolloutStorage(args.num_steps, args.num_processes, obs_shape, envs.action_space, actor_critic.state_size)
+    rollouts = RolloutStorage(args.num_steps, args.num_processes,
+                              obs_shape, envs.action_space, actor_critic.state_size)
     current_obs = torch.zeros(args.num_processes, *obs_shape)
 
     def update_current_obs(obs):
@@ -108,17 +112,20 @@ def main():
         for step in range(args.num_steps):
             # Sample actions
             value, action, action_log_prob, states = actor_critic.act(Variable(rollouts.observations[step], volatile=True),
-                                                                      Variable(rollouts.states[step], volatile=True),
+                                                                      Variable(rollouts.states[
+                                                                               step], volatile=True),
                                                                       Variable(rollouts.masks[step], volatile=True))
             cpu_actions = action.data.squeeze(1).cpu().numpy()
 
             # Obser reward and next obs
             obs, reward, done, info = envs.step(cpu_actions)
-            reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
+            reward = torch.from_numpy(
+                np.expand_dims(np.stack(reward), 1)).float()
             episode_rewards += reward
 
             # If done then clean the history of observations.
-            masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
+            masks = torch.FloatTensor(
+                [[0.0] if done_ else [1.0] for done_ in done])
             final_rewards *= masks
             final_rewards += (1 - masks) * episode_rewards
             episode_rewards *= masks
@@ -132,27 +139,33 @@ def main():
                 current_obs *= masks
 
             update_current_obs(obs)
-            rollouts.insert(step, current_obs, states.data, action.data, action_log_prob.data, value.data, reward, masks)
+            rollouts.insert(step, current_obs, states.data, action.data,
+                            action_log_prob.data, value.data, reward, masks)
 
         next_value = actor_critic(Variable(rollouts.observations[-1], volatile=True),
                                   Variable(rollouts.states[-1], volatile=True),
                                   Variable(rollouts.masks[-1], volatile=True))[0].data
 
-        rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
+        rollouts.compute_returns(
+            next_value, args.use_gae, args.gamma, args.tau)
 
         if args.algo in ['a2c', 'acktr']:
             values, action_log_probs, dist_entropy, states = actor_critic.evaluate_actions(Variable(rollouts.observations[:-1].view(-1, *obs_shape)),
-                                                                                           Variable(rollouts.states[0].view(-1, actor_critic.state_size)),
-                                                                                           Variable(rollouts.masks[:-1].view(-1, 1)),
+                                                                                           Variable(rollouts.states[
+                                                                                                    0].view(-1, actor_critic.state_size)),
+                                                                                           Variable(rollouts.masks[
+                                                                                                    :-1].view(-1, 1)),
                                                                                            Variable(rollouts.actions.view(-1, action_shape)))
 
             values = values.view(args.num_steps, args.num_processes, 1)
-            action_log_probs = action_log_probs.view(args.num_steps, args.num_processes, 1)
+            action_log_probs = action_log_probs.view(
+                args.num_steps, args.num_processes, 1)
 
             advantages = Variable(rollouts.returns[:-1]) - values
             value_loss = advantages.pow(2).mean()
 
-            action_loss = -(Variable(advantages.data) * action_log_probs).mean()
+            action_loss = -(Variable(advantages.data)
+                            * action_log_probs).mean()
 
             if args.algo == 'acktr' and optimizer.steps % optimizer.Ts == 0:
                 # Sampled fisher, see Martens 2014
@@ -164,7 +177,8 @@ def main():
                     value_noise = value_noise.cuda()
 
                 sample_values = values + value_noise
-                vf_fisher_loss = -(values - Variable(sample_values.data)).pow(2).mean()
+                vf_fisher_loss = - \
+                    (values - Variable(sample_values.data)).pow(2).mean()
 
                 fisher_loss = pg_fisher_loss + vf_fisher_loss
                 optimizer.acc_stats = True
@@ -172,46 +186,57 @@ def main():
                 optimizer.acc_stats = False
 
             optimizer.zero_grad()
-            (value_loss * args.value_loss_coef + action_loss - dist_entropy * args.entropy_coef).backward()
+            (value_loss * args.value_loss_coef + action_loss -
+             dist_entropy * args.entropy_coef).backward()
 
             if args.algo == 'a2c':
-                nn.utils.clip_grad_norm(actor_critic.parameters(), args.max_grad_norm)
+                nn.utils.clip_grad_norm(
+                    actor_critic.parameters(), args.max_grad_norm)
 
             optimizer.step()
         elif args.algo == 'ppo':
             advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
-            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+            advantages = (advantages - advantages.mean()) / \
+                (advantages.std() + 1e-5)
 
             for e in range(args.ppo_epoch):
                 if args.recurrent_policy:
                     data_generator = rollouts.recurrent_generator(advantages,
-                                                            args.num_mini_batch)
+                                                                  args.num_mini_batch)
                 else:
                     data_generator = rollouts.feed_forward_generator(advantages,
-                                                            args.num_mini_batch)
+                                                                     args.num_mini_batch)
 
                 for sample in data_generator:
                     observations_batch, states_batch, actions_batch, \
-                       return_batch, masks_batch, old_action_log_probs_batch, \
-                            adv_targ = sample
+                        return_batch, masks_batch, old_action_log_probs_batch, \
+                        adv_targ = sample
 
                     # Reshape to do in a single forward pass for all steps
                     values, action_log_probs, dist_entropy, states = actor_critic.evaluate_actions(Variable(observations_batch),
-                                                                                                   Variable(states_batch),
-                                                                                                   Variable(masks_batch),
+                                                                                                   Variable(
+                                                                                                       states_batch),
+                                                                                                   Variable(
+                                                                                                       masks_batch),
                                                                                                    Variable(actions_batch))
 
                     adv_targ = Variable(adv_targ)
-                    ratio = torch.exp(action_log_probs - Variable(old_action_log_probs_batch))
+                    ratio = torch.exp(action_log_probs -
+                                      Variable(old_action_log_probs_batch))
                     surr1 = ratio * adv_targ
-                    surr2 = torch.clamp(ratio, 1.0 - args.clip_param, 1.0 + args.clip_param) * adv_targ
-                    action_loss = -torch.min(surr1, surr2).mean() # PPO's pessimistic surrogate (L^CLIP)
+                    surr2 = torch.clamp(
+                        ratio, 1.0 - args.clip_param, 1.0 + args.clip_param) * adv_targ
+                    # PPO's pessimistic surrogate (L^CLIP)
+                    action_loss = -torch.min(surr1, surr2).mean()
 
-                    value_loss = (Variable(return_batch) - values).pow(2).mean()
+                    value_loss = (Variable(return_batch) -
+                                  values).pow(2).mean()
 
                     optimizer.zero_grad()
-                    (value_loss + action_loss - dist_entropy * args.entropy_coef).backward()
-                    nn.utils.clip_grad_norm(actor_critic.parameters(), args.max_grad_norm)
+                    (value_loss + action_loss - dist_entropy *
+                     args.entropy_coef).backward()
+                    nn.utils.clip_grad_norm(
+                        actor_critic.parameters(), args.max_grad_norm)
                     optimizer.step()
 
         rollouts.after_update()
@@ -220,17 +245,18 @@ def main():
             end = time.time()
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
             print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
-                format(j, total_num_steps,
-                       int(total_num_steps / (end - start)),
-                       final_rewards.mean(),
-                       final_rewards.median(),
-                       final_rewards.min(),
-                       final_rewards.max(), dist_entropy.data[0],
-                       value_loss.data[0], action_loss.data[0]))
+                  format(j, total_num_steps,
+                         int(total_num_steps / (end - start)),
+                         final_rewards.mean(),
+                         final_rewards.median(),
+                         final_rewards.min(),
+                         final_rewards.max(), dist_entropy.data[0],
+                         value_loss.data[0], action_loss.data[0]))
         if args.vis and j % args.vis_interval == 0:
             try:
                 # Sometimes monitor doesn't properly flush the outputs
-                win = visdom_plot(viz, win, args.log_dir, args.env_name, args.algo)
+                win = visdom_plot(viz, win, args.log_dir,
+                                  args.env_name, args.algo)
             except IOError:
                 pass
 
--- rafiki.py
+++ rafiki.py
@@ -1,7 +1,7 @@
-import time
 import numpy as np
-from scipy import integrate
+# from scipy import integrate
 import random
+import math
 
 
 class Timer:
@@ -22,7 +22,7 @@ class Timer:
         return self.time
 
     def reset(self, start_time=0):
-    	self.time = start_time
+        self.time = start_time
 
 
 class RequestGenerator(object):
@@ -40,11 +40,12 @@ class RequestGenerator(object):
         # the func should range from 0 to 1
         # T = 2*pi/w
 
-    def num_of_new_requests(self, x, delta)
+    def num_of_new_requests(self, x, delta):
         w = 2 * np.pi / self.T
-        num = (math.sin(w * x)) * (self.max_rate - self.min_rate) / 2 + 0.5 * (self.max_rate + self.min_rate)
+        num = (math.sin(w * x)) * (self.max_rate - self.min_rate) / \
+            2 + 0.5 * (self.max_rate + self.min_rate)
         num = int(max(0, num * (1 + random.gauss(0, self.mu))))
-        return num * delta
+        return int(num * delta)
 
     def get(self):
         """
@@ -52,86 +53,96 @@ class RequestGenerator(object):
         :return: (request_id, timestamp)
         """
 
-        # we should keep the avg generating speed around processing model's maximum processing speed, so as to force
-        # the processing is able to process most of the request but can not
-        # process all requests.
         cur_time = self.timer.now()
         # num_float = integrate.quad(self.func, self.last_timestamp, cur_time)[0]
         # to make the requests unifromly located in [last_timestamp, now)
-        num = self.num_of_new_requests(cur_time, cur_time - self.last_timestamp)
-        new_req = [(random.randint(0, self.val_size - 1), random.uniform(self.last_timestamp, cur_time)) for i in range(num)]
-        self.last_timestamp=cur_time
+        num = self.num_of_new_requests(
+            cur_time, cur_time - self.last_timestamp)
+        new_req = [(random.randint(0, self.val_size - 1),
+                    random.uniform(self.last_timestamp, cur_time)) for i in range(num)]
+        self.last_timestamp = cur_time
         return sorted(new_req, key=lambda tup: tup[1])
 
 
 class Discrete:
     # only contains the value of batchsz
-    def __init__(self, num_output)
-        self.n = num_output 
+
+    def __init__(self, num_output):
+        self.n = num_output
 
 
 class Env:
 
-    def __init__(self, batchsz, latency_path, perf_path, obs_size=500):
-        self.latency = np.loadtxt(latency, delimiter=',')
+    def __init__(self, batchsz, latency_path, perf_path, alpha=0, obs_size=500):
+        self.alpha = alpha
+        self.latency = np.loadtxt(latency_path, delimiter=',')
         self.perf = np.loadtxt(perf_path, delimiter=',')
-        self.num_models = latency.shape[0]
+        self.num_models = self.latency.shape[0]
+        nbits = int(math.log2(self.latency.shape[1]))
+        assert (1 << nbits) == self.latency.shape[1], 'num batchsz must be 2^x'
+        assert (1 << self.latency.shape[
+                0]) - 1 == self.perf.shape[0], 'num of models not math perf file'
         self.obs_size = obs_size
         self.state_size = 1
         self.batchsz = batchsz  # a list of candidate batchsz
-        self.waiting_time=np.zeros((num_models, ))
+        self.waiting_time = np.zeros((self.num_models, ))
 
-        assert len(batchsz) == latency.shape[1], 'batchsz %d not match latency shape' % len(batchsz)
+        assert len(batchsz) == self.latency.shape[
+            1], 'batchsz %d not match latency shape' % len(batchsz)
         # the following two items is be compatible with OpenAI gym setting.
-        self.action_space=Discrete(self.num_models + math.log2(latency.shape[1]))
-        self.observation_space= np.zeros((self.obs_size + self.latency.size + self.num_models + 1, ))
+        self.action_space = Discrete(
+            ((1 << self.num_models) - 1) * self.latency.shape[1])
+        self.observation_space = np.zeros(
+            (self.obs_size + self.latency.size + self.num_models + 1, ))
 
         # we use self-defined timer
-        self.timer=Timer(0)
+        self.timer = Timer(0)
 
         # max when all models are running diff data
-        self.max_rate=sum([batchsz[-1] / l[-1] for l in self.latency])
+        self.max_rate = sum([batchsz[-1] / l[-1] for l in self.latency])
         # min when all models are running the same data
-        self.min_rate=min([batchsz[0] / l[0] for l in self.latency])
+        self.min_rate = min([batchsz[0] / l[0] for l in self.latency])
         print('max process rate:', self.max_rate)
         print('min process rate:', self.min_rate)
 
-        self.tau = np.max(latency) * 2
+        self.tau = np.max(self.latency) * 2
         num_of_iters = self.obs_size / batchsz[-1]
-        T = num_of_iters * tau * 10
+        T = num_of_iters * self.tau * 10
         # (pi/2 - asin(0.9)) / pi = 1 / 7 is the peak fraction
 
         # requests generation model, we use it to generate requests.
-        self.requests=[]
-        self.requests_gen=RequestGenerator(self.timer, 5000, self.max_rate, self.min_rate, T)
+        self.requests = []
+        self.requests_gen = RequestGenerator(
+            self.timer, 5000, self.max_rate, self.min_rate, T)
         self.reset()
 
     def parse_action(self, action):
-        # first num_models bits for model selection; the rest bits for batchsz index
-        b = [int(x) for x in bin(action)[2:]]
-        model_idx = b[0:len(self.num_models)]
-        batchsz_idx = np.array(b[len(num_models):])
-        batchsz_idx=batchsz_idx.dot(1 << np.arange(batchsz_idx.size)[::-1])
-        return np.where(model_idx>0)[0], batchsz_idx
+        # first num_models bits for model selection; the rest bits for batchsz
+        # index
+        action = int(action)
+        model_part = action // self.latency.shape[1] + 1
+        batchsz_idx = action % self.latency.shape[1]
+        model_idx = np.array([int(x) for x in bin(model_part)[2:]])
+        return np.where(model_idx > 0)[0], batchsz_idx
 
     def step(self, action):
         """
           :return: obs s1 and cost c1
         """
-        model_idx, batchsz_idx=self.parse_action(action)
+        model_idx, batchsz_idx = self.parse_action(action)
         self.waiting_time[model_idx] += self.latency[model_idx, batchsz_idx]
         batchsz = self.batchsz[batchsz_idx]
-        num_overdue=0
+        num_overdue = 0
         cur_time = self.timer.now()
         max_waiting_time = np.max(self.waiting_time[model_idx])
         for _, inqueue_time in self.requests[:batchsz]:
-            latency= cur_time - inqueue_time + max_waiting_time
+            latency = cur_time - inqueue_time + max_waiting_time
             if latency > self.tau:
                 num_overdue += 1
         acc = self.accuracy(self.requests[:batchsz],  model_idx)
-        self.requests= self.requests[batchsz:]
+        self.requests = self.requests[batchsz:]
         reward = acc * batchsz - self.alpha * num_overdue
-        delta=np.min(self.waiting_time)
+        delta = np.min(self.waiting_time)
         self.timer.tick(delta)
         self.waiting_time -= delta
         self.update_obs()
@@ -141,29 +152,34 @@ class Env:
     def update_obs(self):
         new_req = self.requests_gen.get()
         total_size = len(new_req) + len(self.requests)
-        assert total_size < 10*self.obs_size, 'too many requests %d' % total_size
-    	self.requests.extend(new_req)
+        assert total_size < 10 * self.obs_size, 'too many requests %d' % total_size
+        self.requests.extend(new_req)
         size = min(self.obs_size, total_size)
-        self.obs = np.zeros((self.obs_size + self.latency.size + self.num_models + 1,))
+        self.obs = np.zeros(
+            (self.obs_size + self.latency.size + self.num_models + 1,))
         self.obs[0] = self.tau
-        self.obs[1:1+self.latency.size] = self.latency.reshape((-1,))
+        self.obs[1:1 + self.latency.size] = self.latency.reshape((-1,))
         offset = 1 + self.latency.size + self.num_models
-        self.obs[1+self.latency.size: offset] = self.waiting_time
-        self.obs[offset: offset+size] = self.timer.now() - np.array([r[1] for r in self.requests[:size]])
+        self.obs[1 + self.latency.size: offset] = self.waiting_time
+        self.obs[offset: offset + size] = self.timer.now() - \
+            np.array([r[1] for r in self.requests[:size]])
 
     def accuracy(self, requests, model_idx):
-        comb_id=model_idx.dot(1 << np.arange(model_idx.size)[::-1])
+        comb_id = model_idx.dot(1 << np.arange(model_idx.size)[::-1]) - 1
         return self.perf[comb_id]
 
     def reset(self):
-    	self.timer.reset()
-    	self.timer.tick(self.tau)
-        return self.update_obs(self)
+        self.timer.reset()
+        self.timer.tick(self.tau)
+        return self.update_obs()
 
 
 class Envs(object):
+
     def __init__(self, num_processes):
-        self.envs = [ Env(range(16, 65, 16), 'latency.npy', 'perf.npy', obs_size=500) for i in range(num_processes)]
+        self.num_processes = num_processes
+        self.envs = [Env(range(16, 65, 16), 'latency.txt',
+                         'accuracy.txt', obs_size=500) for i in range(num_processes)]
         self.observation_space = self.envs[0].observation_space
         self.action_space = self.envs[0].action_space
 
@@ -174,14 +190,14 @@ class Envs(object):
         info = []
         for k, env in enumerate(self.envs):
             o, r, d, s = env.step(actions[k])
-            obs[i, :] = o
-            reward[i] = r
-            done[i] = d
+            obs[k, :] = o
+            reward[k] = r
+            done[k] = d
             info.append(s)
         return obs, reward, done, info
-
 
     def reset(self):
         ret = np.empty((self.num_processes, self.observation_space.shape[0]))
         for i, env in enumerate(self.envs):
             ret[i, :] = env.reset()
+        return ret
