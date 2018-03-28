c3dd35a01008f02d87ff2752f066482624ee3163
Wang Wei <dcswaw@nus.edu.sg>
2018-03-28 16:33:40 +0800

    add comments for rafiki.py

--- main.py
+++ main.py
@@ -69,22 +69,11 @@ def main():
 
     os.environ['OMP_NUM_THREADS'] = '1'
 
-    if args.vis:
-        from visdom import Visdom
-        viz = Visdom(port=args.port)
-        win = None
-
-    envs = rafiki.Envs(args.num_processes, args.policy, args.obs_size)
+    envs = rafiki.Envs(args.num_processes, args.num_models, args.policy,
+                       args.obs_size, args.cycle_len)
     obs_shape = envs.observation_space.shape
-    obs_shape = (obs_shape[0] * args.num_stack, *obs_shape[1:])
 
-    if len(envs.observation_space.shape) == 3:
-        actor_critic = CNNPolicy(
-            obs_shape[0], envs.action_space, args.recurrent_policy)
-    else:
-        assert not args.recurrent_policy, \
-            "Recurrent policy is not implemented for the MLP controller"
-        actor_critic = MLPPolicy(obs_shape[0], envs.action_space)
+    actor_critic = MLPPolicy(obs_shape[0], envs.action_space)
 
     if envs.action_space.__class__.__name__ == "Discrete":
         action_shape = 1
@@ -110,18 +99,14 @@ def main():
     def update_current_obs(obs):
         shape_dim0 = envs.observation_space.shape[0]
         obs = torch.from_numpy(obs).float()
-        if args.num_stack > 1:
-            current_obs[:, :-shape_dim0] = current_obs[:, shape_dim0:]
         current_obs[:, -shape_dim0:] = obs
 
     obs = envs.reset()
     update_current_obs(obs)
-
     rollouts.observations[0].copy_(current_obs)
 
     # These variables are used to compute average rewards for all processes.
-    episode_rewards = torch.zeros([args.num_processes, 1])
-    final_rewards = torch.zeros([args.num_processes, 1])
+    # final_rewards = torch.zeros([args.num_processes, 1])
 
     if args.cuda:
         current_obs = current_obs.cuda()
@@ -136,28 +121,15 @@ def main():
                                                                                step], volatile=True),
                                                                       Variable(rollouts.masks[step], volatile=True))
             cpu_actions = action.data.squeeze(1).cpu().numpy()
-
             # Obser reward and next obs
             obs, reward, done, info = envs.step(cpu_actions)
             reward = torch.from_numpy(
                 np.expand_dims(np.stack(reward), 1)).float()
-            episode_rewards += reward
-
-            # If done then clean the history of observations.
+            # final_rewards += rewards
             masks = torch.FloatTensor(
                 [[0.0] if done_ else [1.0] for done_ in done])
-            final_rewards *= masks
-            final_rewards += (1 - masks) * episode_rewards
-            episode_rewards *= masks
-
             if args.cuda:
                 masks = masks.cuda()
-
-            if current_obs.dim() == 4:
-                current_obs *= masks.unsqueeze(2).unsqueeze(2)
-            else:
-                current_obs *= masks
-
             update_current_obs(obs)
             rollouts.insert(step, current_obs, states.data, action.data,
                             action_log_prob.data, value.data, reward, masks)
@@ -264,21 +236,8 @@ def main():
         if j % args.log_interval == 0:
             end = time.time()
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
-            print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
-                  format(j, total_num_steps,
-                         int(total_num_steps / (end - start)),
-                         final_rewards.mean(),
-                         final_rewards.median(),
-                         final_rewards.min(),
-                         final_rewards.max(), dist_entropy.data[0],
-                         value_loss.data[0], action_loss.data[0]))
-        if args.vis and j % args.vis_interval == 0:
-            try:
-                # Sometimes monitor doesn't properly flush the outputs
-                win = visdom_plot(viz, win, args.log_dir,
-                                  args.env_name, args.algo)
-            except IOError:
-                pass
+            print("Updates {}, num timesteps {}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
+                  format(j, total_num_steps, dist_entropy.data[0], value_loss.data[0], action_loss.data[0]))
 
 if __name__ == "__main__":
     main()
--- rafiki.py
+++ rafiki.py
@@ -35,21 +35,25 @@ class Timer:
 
 class RequestGenerator(object):
 
-    def __init__(self, timer, val_size, rate, T, sigma=0.1, mu=0.01):
+    def __init__(self, timer, rate, T, sigma=0.1, mu=0.01, seed=1):
+        # timer, is shared with the env
+        # rate, is the reference throughput
         # self.cur_req_id = 1
         self.timer = timer
-        self.val_size = val_size
         self.T = T
         self.mu = mu
+        # to make 20% time the arriving rate is larger than the reference throughput (rate)
         # sin(0.5pi-0.2pi)*k + b = r; sin(0.5pi)*k + b = (1+sigma)*r
         self.k = sigma * rate * 5
         self.b = (1 + sigma) * rate - self.k
 
         self.last_timestamp = timer.now()
+        random.seed(seed)
         # the func should range from 0 to 1
         # T = 2*pi/w
 
     def num_of_new_requests(self, delta):
+        # 20% time the arriving rate is larger than the reference throughput
         x = self.timer.now()
         w = 2 * np.pi / self.T
         num = math.sin(w * x) * self.k + self.b
@@ -69,9 +73,10 @@ class RequestGenerator(object):
         # num_float = integrate.quad(self.func, self.last_timestamp, cur_time)[0]
         # to make the requests unifromly located in [last_timestamp, now)
         num = self.num_of_new_requests(cur_time - self.last_timestamp)
-        new_req = [(random.randint(0, self.val_size - 1),
+        new_req = [(random.randint(0, 4999),  # hard code the request id range, here
                     random.uniform(self.last_timestamp, cur_time)) for i in range(num)]
         self.last_timestamp = cur_time
+        # sort the requests based on inqueue time
         return sorted(new_req, key=lambda tup: tup[1])
 
 
@@ -81,21 +86,22 @@ class Discrete:
     def __init__(self, num_output):
         self.n = num_output
 
+# to label the logger of different env
 env_id = 0
 
 
 class Env:
 
-    def __init__(self, requests_gen, timer, batchsz, tau, alpha=1, obs_size=500):
+    def __init__(self, requests_gen, timer, batchsz, tau, latency, perf, alpha=0.5, obs_size=50):
         self.requests_gen = requests_gen
         self.timer = timer
         self.batchsz = batchsz  # a list of candidate batchsz
-        self.tau = tau
-        self.alpha = alpha
-        self.obs_size = obs_size
+        self.tau = tau  # time limit of each request
+        self.alpha = alpha  # coefficient of the overdue requests in the reward function
+        self.obs_size = obs_size  # valid queue length for queue feature into the RL model
 
-        self.latency = np.loadtxt('latency.txt', delimiter=',')
-        self.perf = np.loadtxt('accuracy.txt', delimiter=',')
+        self.latency = latency  # a matrix with one row per model, one column per batchsz
+        self.perf = perf  # a list performance/accuracy for (ensemble) model
 
         self.num_models = self.latency.shape[0]
         self.num_batchsz = self.latency.shape[1]
@@ -107,23 +113,36 @@ class Env:
 
         assert len(batchsz) == self.num_batchsz, \
             'batchsz %d not match latency shape' % len(batchsz)
-        # the following two items is be compatible with OpenAI gym setting.
+        # 2^(self.num_models) includes all ensemble combinations. we manually
+        # exclude the case where no model is selected in the action
+        # the action for model selection and for batchsz selection is merged
+        # with the first num_models bits for model selection and the last
+        # log2(num_batchsz) for batch selection.
         self.action_space = Discrete(
             ((1 << self.num_models) - 1) * self.num_batchsz)
+
+        # the obs space includes the tau, latency for all models and all
+        # batchsz, waiting time of each model to finish existing requests and
+        # the queue states (queuing time)
         self.observation_space = np.zeros(
             (self.obs_size + self.latency.size + self.num_models + 1, ))
-        # requests generation model, we use it to generate requests.
-        self.reset()
+
+        # self.reset()
         global env_id
         self.logger = logging.getLogger('Rafiki.env-%d' % env_id)
         env_id += 1
 
     def model_idx_to_model_action(self, model_idx):
+        # convert model selection into model action part
+        # model_idx is a binary array, pos k = 1 indicating k-th model is selected
+        # - 1 is to exclude [0,0,0...0]
         return model_idx.dot(1 << np.arange(model_idx.size)[::-1]) - 1
 
     def model_action_to_model_idx(self, action):
+        # extract model selection from the model action part
         bstr = bin(action + 1)
-        pad = [False] * (self.num_models - (len(bstr) - 2))
+        pad = [False] * (self.num_models - (len(bstr) - 2)
+                         )  # pad with 0 on the left
         model_idx = np.array(pad + [bool(int(x)) for x in bstr[2:]])
         return model_idx
 
@@ -131,6 +150,7 @@ class Env:
         return self.perf[self.model_idx_to_model_action(model_idx)]
 
     def parse_action(self, action):
+        # parse full action from RL output into model_idx array and batchsz index
         # first num_models bits for model selection; the rest bits for batchsz
         # index; action value for models selection starting from 1 (0 means
         # no model is selected)
@@ -140,6 +160,7 @@ class Env:
         return model_idx > 0, batchsz_idx
 
     def create_action(self, model_idx, batchsz_idx):
+        # reverse op of parse_action
         nbits = int(math.log2(self.num_batchsz))
         action = self.model_idx_to_model_action(model_idx) << nbits
         action += batchsz_idx
@@ -150,42 +171,56 @@ class Env:
           :return: obs s1 and cost c1
         """
         model_idx, batchsz_idx = self.parse_action(action)
+
+        # inc the processing time of the selected models
         self.waiting_time[model_idx] += self.latency[model_idx, batchsz_idx]
+
         batchsz = self.batchsz[batchsz_idx]
         num_overdue = 0
         cur_time = self.timer.now()
+        # the latency of this batch of requests depends on the slowest model
         max_waiting_time = np.max(self.waiting_time[model_idx])
+
         num = min(batchsz, len(self.requests))
         for _, inqueue_time in self.requests[:num]:
             latency = cur_time - inqueue_time + max_waiting_time
             if latency > self.tau:
                 num_overdue += 1
         acc = self.accuracy(self.requests[:num], model_idx)
-        self.requests = self.requests[num:]
+
         reward = acc * num - self.alpha * acc * num_overdue
+
+        # printing
+        delta = self.timer.now() - self.last_log_time
+        self.logr += reward
+        self.logt += num
+        self.logo += num_overdue
+        self.loga += acc * num
+        if delta >= 1:
+            self.logger.info('time %5.1f, reward %5.1f, acc %5.3f, overdue %5.1f, throughput %5.1f, arr rate %5.1f, queue size %d, batchsz %d' %
+                             (self.timer.now(), self.logr / delta,
+                              self.loga / self.logt, self.logo / delta,
+                              self.logt / delta,
+                              self.requests_gen.num_of_new_requests(1),
+                              len(self.requests), batchsz))
+            self.last_log_time = self.timer.now()
+            self.logr, self.logt, self.logo, self.loga = 0, 0, 0, 0
+
+        # update timer to proceed with the next RL iter
         if sync:
             delta = self.waiting_time
             self.timer.tick(np.max(delta))
         else:
             delta = np.min(self.waiting_time)
             self.timer.tick(delta)
+
+        # delta time has passed
         self.waiting_time -= delta
+        # delete the dispatched requests from the queue
+        self.requests = self.requests[num:]
+        # update env queue status with new requests
         self.update_obs()
         # obs, reward, done, _
-        self.logr += reward
-        self.logt += num
-        self.logo += num_overdue
-        self.loga += acc * num
-        delta = self.timer.now() - self.last_log_time
-        if delta >= 1:
-            self.logger.info('time %5.1f, reward %5.1f, acc %3.2f, overdue %5.1f, throughput %5.1f, arr rate %5.1f, queue size %d' %
-                             (self.timer.now(), self.logr / delta,
-                              self.loga / self.logt, self.logo / delta,
-                              self.logt / delta,
-                              self.requests_gen.num_of_new_requests(1),
-                              len(self.requests)))
-            self.last_log_time = self.timer.now()
-            self.logr, self.logt, self.logo, self.loga = 0, 0, 0, 0
         return self.obs, reward, False, \
             {'acc': acc, 'overdue': num_overdue, 'batchsz': num,
              'num_models': sum(model_idx), 'time': cur_time}
@@ -193,10 +228,12 @@ class Env:
     def update_obs(self):
         new_req = self.requests_gen.get()
         total_size = len(new_req) + len(self.requests)
-        assert total_size < 10 * self.obs_size, \
+        assert total_size < 100 * self.obs_size, \
             'too many requests %d' % total_size
         self.requests.extend(new_req)
         size = min(self.obs_size, total_size)
+
+        # preare obserations for RL algorithm
         self.obs = np.zeros(
             (self.obs_size + self.latency.size + self.num_models + 1,))
         self.obs[0] = self.tau
@@ -207,6 +244,7 @@ class Env:
             np.array([r[1] for r in self.requests[:size]])
 
     def reset(self):
+        # must be called after init env
         self.timer.reset()
         self.requests_gen.reset()
         self.requests = []
@@ -219,11 +257,13 @@ class Env:
 
 
 class Envs(object):
+    # a list of envs
 
-    def __init__(self, num_processes, policy, obs_size):
+    def __init__(self, num_processes, num_models, policy, obs_size, cycle=200):
         self.num_processes = num_processes
-        batchsz = range(16, 65, 16)
-        latency = np.loadtxt('latency.txt', delimiter=',')
+        batchsz = range(16, 65, 16)[:num_models]
+        latency = np.loadtxt('latency.txt', delimiter=',')[:num_models]
+        perf = np.loadtxt('accuracy.txt', delimiter=',')[:num_models]
         max_rate = sum([batchsz[-1] / l[-1] for l in latency])
         # min when all models are running the same data
         min_rate = min([batchsz[0] / l[0] for l in latency])
@@ -231,8 +271,8 @@ class Envs(object):
         logger.info('min process rate: %f' % min_rate)
 
         tau = np.max(latency) * 2
-        num_of_iters = obs_size / batchsz[-1]
-        T = num_of_iters * tau * 10
+        # num_of_iters = obs_size / batchsz[-1]
+        T = tau * cycle
         logger.info('sin cycle %f' % T)
         # (pi/2 - asin(0.9)) / pi = 1 / 7 is the peak fraction
         self.envs = []
@@ -241,11 +281,11 @@ class Envs(object):
             if policy == 'sync':
                 requests_gen = RequestGenerator(timer, 5000, min_rate, T)
                 env = Env(requests_gen, timer, batchsz,
-                          tau=tau, obs_size=obs_size)
+                          tau, latency, perf, obs_size=obs_size)
             else:
                 requests_gen = RequestGenerator(timer, 5000, max_rate, T)
                 env = Env(requests_gen, timer, batchsz,
-                          tau=tau, obs_size=obs_size)
+                          tau, latency, perf, obs_size=obs_size)
             self.envs.append(env)
         self.observation_space = self.envs[0].observation_space
         self.action_space = self.envs[0].action_space
@@ -271,12 +311,18 @@ class Envs(object):
 
 
 def step(env, model_idx, sync):
-     # choose biggest batchsz and smallest waiting queue
+    # greedy algorithm
+    # choose biggest batchsz and smallest waiting queue
     action = None
     while action is None:
         tick = False
         for k, bs in zip(range(env.num_batchsz)[::-1], env.batchsz[::-1]):
             if bs <= len(env.requests):
+                '''
+                if bs < len(env.requests) and (k + 1) < env.num_batchsz:
+                    action = env.create_action(model_idx, k + 1)
+                else:
+                '''
                 action = env.create_action(model_idx, k)
                 break
             elif len(env.requests) == 0 or \
@@ -310,13 +356,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Request serving policy optimization.')
     parser.add_argument(
-        '--policy', choices=['async', 'sync'], default='rl', help='policy')
-    parser.add_argument('--obs_size', type=int, default=500,
+        '--policy', choices=['async', 'sync'], default='sync', help='policy')
+    parser.add_argument('--obs_size', type=int, default=50,
                         help='observation vector size')
-    parser.add_argument('--sigma', type=float, default=0.1,
-                        help='variation of the min/max rate')
     parser.add_argument('--epoch', type=int, default=3)
     parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--num_models', type=int, default=3)
+    parser.add_argument('--cycle_len', type=int, default=500)
 
     args = parser.parse_args()
 
@@ -342,7 +388,8 @@ if __name__ == '__main__':
     logger.info(args)
 
     batchsz = range(16, 65, 16)
-    latency = np.loadtxt('latency.txt', delimiter=',')
+    latency = np.loadtxt('latency.txt', delimiter=',')[:args.num_models]
+    perf = np.loadtxt('accuracy.txt', delimiter=',')[:args.num_models]
     max_rate = sum([batchsz[-1] / l[-1] for l in latency])
     # min when all models are running the same data
     min_rate = min([batchsz[0] / l[0] for l in latency])
@@ -350,18 +397,22 @@ if __name__ == '__main__':
     logger.info('min process rate: %f' % min_rate)
 
     tau = np.max(latency) * 2
-    num_of_iters = args.obs_size / batchsz[-1]
-    T = num_of_iters * tau * 10
+    # num_of_iters = args.obs_size / batchsz[-1]
+    T = tau * args.cycle_len
     logger.info('sin cycle %f' % T)
     # (pi/2 - asin(0.9)) / pi = 1 / 7 is the peak fraction
     timer = Timer()
     if args.policy == 'sync':
+        # always select all models
         requests_gen = RequestGenerator(timer, 5000, min_rate, T)
         env = Env(requests_gen, timer, batchsz,
-                  tau=tau, obs_size=args.obs_size)
+                  tau, latency, perf, obs_size=args.obs_size)
+        env.reset()
         sync_run(env, args.epoch * T)
     else:
+        # select the free model
         requests_gen = RequestGenerator(timer, 5000, max_rate, T)
         env = Env(requests_gen, timer, batchsz,
-                  tau=tau, obs_size=args.obs_size)
+                  tau, latency, perf, obs_size=args.obs_size)
+        env.reset()
         async_run(env, args.epoch * T)
